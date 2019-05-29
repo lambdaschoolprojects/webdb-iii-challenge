07e58b2577a4ea5d027ef379215b81bf5b07e5d2
RaneWallin <rane@ranewallin.com>
2019-05-28 19:53:28 -0700

    added knex connection

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const knex = require('knex');
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/lambda.db3',
+    },
+    useNullAsDefault: true,
+}
+
+const db = knex(knexConfig);
 
 const server = express();
 
